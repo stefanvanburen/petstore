58d9d971c110e4500fc543afb54f959097ef7f75
Stefan VanBuren <stefan@vanburen.xyz>
2023-02-25 10:29:20 -0500

    Remove test dependency

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/stefanvanburen/petstore
 go 1.19
 
 require (
-	github.com/akshayjshah/attest v0.4.0
 	github.com/bufbuild/connect-go v1.1.0
 	github.com/oklog/ulid/v2 v2.1.0
 	github.com/rs/cors v1.8.2
@@ -12,7 +11,6 @@ require (
 )
 
 require (
-	github.com/google/go-cmp v0.5.9 // indirect
 	go.buf.build/bufbuild/connect-go/acme/paymentapis v1.10.3 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/akshayjshah/attest"
 	"github.com/bufbuild/connect-go"
 	petv1 "go.buf.build/bufbuild/connect-go/acme/petapis/pet/v1"
 )
@@ -23,28 +22,48 @@ func TestServer(t *testing.T) {
 		PetType: givenPet.PetType,
 		Name:    givenPet.Name,
 	}))
-	attest.Ok(t, err)
-	attest.Equal(t, putPetResponse.Msg.Pet.Name, givenPet.Name)
-	attest.Equal(t, putPetResponse.Msg.Pet.PetType, givenPet.PetType)
+	if err != nil {
+		t.Errorf("PutPet: got %v, want err = nil", err)
+	}
+	gotPutPet := putPetResponse.Msg.Pet
+	if gotPutPet.Name != givenPet.Name {
+		t.Errorf("PutPet: got %s name, want %s name", gotPutPet.Name, givenPet.Name)
+	}
+	if gotPutPet.PetType != givenPet.PetType {
+		t.Errorf("PutPet: got %v type, want %v type", gotPutPet.PetType, givenPet.PetType)
+	}
 
 	petID := putPetResponse.Msg.Pet.PetId
 
 	getPetResponse, err := server.GetPet(ctx, connect.NewRequest(&petv1.GetPetRequest{
 		PetId: petID,
 	}))
-	attest.Ok(t, err)
-	attest.Equal(t, getPetResponse.Msg.Pet.Name, givenPet.Name)
-	attest.Equal(t, getPetResponse.Msg.Pet.PetType, givenPet.PetType)
+	if err != nil {
+		t.Errorf("GetPet: got %v, want err = nil", err)
+	}
+	gotGetPet := getPetResponse.Msg.Pet
+	if gotGetPet.Name != givenPet.Name {
+		t.Errorf("GetPet: got %s name, want %s name", gotGetPet.Name, givenPet.Name)
+	}
+	if gotGetPet.PetType != givenPet.PetType {
+		t.Errorf("GetPet: got %v type, want %v type", gotGetPet.PetType, givenPet.PetType)
+	}
 
 	_, err = server.DeletePet(ctx, connect.NewRequest(&petv1.DeletePetRequest{
 		PetId: petID,
 	}))
-	attest.Ok(t, err)
+	if err != nil {
+		t.Errorf("DeletePet: got %v, want err = nil", err)
+	}
 
 	_, err = server.GetPet(ctx, connect.NewRequest(&petv1.GetPetRequest{
 		PetId: putPetResponse.Msg.Pet.PetId,
 	}))
 	var connectErr *connect.Error
-	attest.True(t, errors.As(err, &connectErr))
-	attest.Equal(t, connectErr.Code(), connect.CodeNotFound)
+	if isConnectErr := errors.As(err, &connectErr); !isConnectErr {
+		t.Errorf("GetPet: got %v, want connectErr", err)
+	}
+	if connectErr.Code() != connect.CodeNotFound {
+		t.Errorf("GetPet: got %v code, want connect.CodeNotFound", connectErr.Code())
+	}
 }
