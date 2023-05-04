e2c12eacff95436d79c7b772c205c40ad13d8e4b
Stefan VanBuren <svanburen@buf.build>
2023-05-04 14:43:43 -0400

    Serve README.md as HTML at root

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 
 require (
 	buf.build/gen/go/acme/paymentapis/protocolbuffers/go v1.30.0-20220907172603-9a877cf260e1.1 // indirect
+	github.com/gomarkdown/markdown v0.0.0-20230322041520-c84983bdbf2a // indirect
 	golang.org/x/exp v0.0.0-20230213192124-5e25df0256eb // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	_ "embed"
+	"fmt"
 	"log"
 	"net/http"
 
 	"buf.build/gen/go/acme/petapis/bufbuild/connect-go/pet/v1/petv1connect"
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/parser"
 	"github.com/jub0bs/fcors"
 	"github.com/stefanvanburen/petstore/internal/server"
 )
 
+//go:embed README.md
+var readmeMarkdown []byte
+
 func main() {
-	petServer := server.NewPetServer()
+	path, handler := petv1connect.NewPetStoreServiceHandler(server.NewPetServer())
 	mux := http.NewServeMux()
-	path, handler := petv1connect.NewPetStoreServiceHandler(petServer)
 	mux.Handle(path, handler)
-	mux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		w.WriteHeader(http.StatusOK)
+	mux.HandleFunc("/", func(responseWriter http.ResponseWriter, _ *http.Request) {
+		responseWriter.Write(markdownToHTML(readmeMarkdown))
 	})
 	cors, err := fcors.AllowAccess(
 		fcors.FromOrigins("https://studio.buf.build"),
@@ -26,7 +33,14 @@ func main() {
 		),
 	)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(fmt.Errorf("setting up CORS: %s", err))
 	}
 	http.ListenAndServe(":8080", cors(mux))
+}
+
+func markdownToHTML(markdownContent []byte) []byte {
+	return markdown.Render(
+		parser.New().Parse(markdownContent),
+		html.NewRenderer(html.RendererOptions{Flags: html.CommonFlags}),
+	)
 }
