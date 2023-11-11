d7e9be6148f307a19ecbdd7f7e8b99b581c93ecf
Stefan VanBuren <stefan@vanburen.xyz>
2023-11-11 18:07:14 -0500

    Use attest for tests
    Reduces some noise in the tests.

--- go.mod
+++ go.mod
@@ -17,7 +17,9 @@ require (
 
 require (
 	buf.build/gen/go/acme/paymentapis/protocolbuffers/go v1.31.0-20220907172603-9a877cf260e1.1 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/safehtml v0.0.2 // indirect
+	go.akshayshah.org/attest v1.0.2 // indirect
 	golang.org/x/exp v0.0.0-20230801115018-d63ba01acd4b // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
--- internal/petstoreservice/petstoreservice_test.go
+++ internal/petstoreservice/petstoreservice_test.go
@@ -7,6 +7,7 @@ import (
 
 	petv1 "buf.build/gen/go/acme/petapis/protocolbuffers/go/pet/v1"
 	"connectrpc.com/connect"
+	"go.akshayshah.org/attest"
 )
 
 func TestPetStoreService(t *testing.T) {
@@ -22,48 +23,31 @@ func TestPetStoreService(t *testing.T) {
 		PetType: givenPet.PetType,
 		Name:    givenPet.Name,
 	}))
-	if err != nil {
-		t.Errorf("PutPet: got %v, want err = nil", err)
-	}
+	attest.Ok(t, err)
 	gotPutPet := putPetResponse.Msg.Pet
-	if gotPutPet.Name != givenPet.Name {
-		t.Errorf("PutPet: got %s name, want %s name", gotPutPet.Name, givenPet.Name)
-	}
-	if gotPutPet.PetType != givenPet.PetType {
-		t.Errorf("PutPet: got %v type, want %v type", gotPutPet.PetType, givenPet.PetType)
-	}
+	attest.Equal(t, gotPutPet.Name, givenPet.Name)
+	attest.Equal(t, gotPutPet.PetType, givenPet.PetType)
 
 	petID := putPetResponse.Msg.Pet.PetId
 
 	getPetResponse, err := petstoreservice.GetPet(ctx, connect.NewRequest(&petv1.GetPetRequest{
 		PetId: petID,
 	}))
-	if err != nil {
-		t.Errorf("GetPet: got %v, want err = nil", err)
-	}
+	attest.Ok(t, err)
 	gotGetPet := getPetResponse.Msg.Pet
-	if gotGetPet.Name != givenPet.Name {
-		t.Errorf("GetPet: got %s name, want %s name", gotGetPet.Name, givenPet.Name)
-	}
-	if gotGetPet.PetType != givenPet.PetType {
-		t.Errorf("GetPet: got %v type, want %v type", gotGetPet.PetType, givenPet.PetType)
-	}
+	attest.Equal(t, gotGetPet.Name, givenPet.Name)
+	attest.Equal(t, gotGetPet.PetType, givenPet.PetType)
 
 	_, err = petstoreservice.DeletePet(ctx, connect.NewRequest(&petv1.DeletePetRequest{
 		PetId: petID,
 	}))
-	if err != nil {
-		t.Errorf("DeletePet: got %v, want err = nil", err)
-	}
+	attest.Ok(t, err)
 
 	_, err = petstoreservice.GetPet(ctx, connect.NewRequest(&petv1.GetPetRequest{
 		PetId: putPetResponse.Msg.Pet.PetId,
 	}))
 	var connectErr *connect.Error
-	if isConnectErr := errors.As(err, &connectErr); !isConnectErr {
-		t.Errorf("GetPet: got %v, want connectErr", err)
-	}
-	if connectErr.Code() != connect.CodeNotFound {
-		t.Errorf("GetPet: got %v code, want connect.CodeNotFound", connectErr.Code())
-	}
+	isConnectErr := errors.As(err, &connectErr)
+	attest.True(t, isConnectErr)
+	attest.Equal(t, connectErr.Code(), connect.CodeNotFound)
 }
